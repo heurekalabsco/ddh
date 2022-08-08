539efe9397570d35e1031ab17e90e00581894850
matthewhirschey <matthew@hirschey.org>
2022-08-08 11:17:03 -0400

    update report message fun
    add json_array to better handle params

--- R/helper.R
+++ R/helper.R
@@ -781,53 +781,6 @@ make_legend <- function(fun,
   return(legend)
 }
 
-#' Send Report Message
-#'
-#' \code{send_report_message} generates a message that gets sent to the AWS simple queue service (SQS) for report generation
-#'
-#' This is an emailing function that sends a message to the SQS queue for email generation
-#'
-#' @param first_name First name of the report addressee, which is used in the report email body
-#' @param last_name Last name of the report addressee.
-#' @param email_address Email address for the report to be delivered to
-#' @param input A list containing type, query, and content variables.
-#' @param private A Boolean determining which information to include in the report
-#'
-#' @importFrom magrittr %>%
-#'
-#' @export
-#' @examples
-#' send_report_message(first_name = "Matthew", last_name = "Hirschey", email_address = "matthew_at_hirschey.org", input = list(type = "gene", query = "ROCK1", content = "ROCK1"), private = TRUE)
-
-#' \dontrun{
-#' send_report_message(first_name = "Matthew", last_name = "Hirschey", email_address = "matthew_at_hirschey.org", input = list(type = "gene", query = "ROCK1", content = "ROCK1"), private = TRUE)
-#' }
-send_report_message <- function(first_name,
-                                last_name,
-                                email_address,
-                                input = list(),
-                                private){
-  json_array <-
-    tibble::tibble(
-      first_name = first_name,
-      last_name = last_name,
-      email_address = email_address,
-      type = input$type,
-      subtype = input$subtype,
-      query = stringr::str_c(input$query, collapse = ", "),
-      content = stringr::str_c(input$content, collapse = ", "),
-      private = private
-    ) %>%
-    jsonlite::toJSON(dataframe = "rows")
-
-  sqs <- paws::sqs()
-  sqs$send_message(
-    QueueUrl = Sys.getenv("AWS_SQS_SERVICE_URL"),
-    MessageBody = json_array
-  )
-  message(glue::glue("{glue::glue_collapse(input$query, sep = ', ')} sqs message sent for {first_name} ({email_address})"))
-}
-
 #DATA GENERATION----
 #' Fix names
 #'
