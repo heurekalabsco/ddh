04747359798c481bd46e3b6aa1a73c18fdc14461
matthewhirschey <matthew@hirschey.org>
2022-08-08 16:07:05 -0400

    add make_example function

--- R/helper.R
+++ R/helper.R
@@ -1211,3 +1211,28 @@ cell_linkr <- function(query, type) {
   }
 }
 
+###make example
+#' Function to create an HTML string of examples to use on the index page and the start here methods doc
+#'
+#' @param privateMode Boolean indicating if private data is pointed to
+#' @return An HTML string.
+#'
+#' @examples
+#' make_example()
+#'
+#' @export
+make_example <- function(privateMode = TRUE){
+  #need to dynamically switch depending on privateMode
+  tld <- dplyr::if_else(privateMode == TRUE, "com", "org")
+  url <- glue::glue('http://www.datadrivenhypothesis.{tld}/')
+
+  #need full urls so works in methods, instead of reference url
+  examples <- glue::glue('<h4>Search for genes</h5>
+              <ul>
+                <li>A single gene, such as <a href="{url}?show=gene&query=TP53">TP53</a> or <a href="{url}?show=gene&query=BRCA1">BRCA1</a></li>
+                <li>A pathway name, such as <a href="{url}?show=search&query=cholesterol">cholesterol</a>, which will lead you to <a href="{url}?show=pathway&query=0006695">Cholesterol Biosynthetic Process</a></li>
+                <li>The Gene Ontology biological process identifier, such as <a href="{url}?show=search&query=1901989">1901989</a>, which will find <a href="{url}?show=pathway&query=1901989">Pathway: Positive Regulation Of Cell Cycle Phase Transition (GO:1901989)</a></li>
+                <li>A custom list of genes (separated by commas), such as <a href="{url}?show=search&query=BRCA1,%20BRCA2">BRCA1, BRCA2</a>, which will search <a href="{url}?show=gene_list&query=BRCA1,BRCA2">a custom gene list</a></li>
+              </ul>')
+  return(examples)
+}
