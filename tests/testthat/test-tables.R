060c7932c683d701dd1865c5d2e832c1d2ccb35c
John Bradley <johnbradley2008@gmail.com>
2023-04-14 11:45:42 -0400

    Add get_gene_symbols_for_pathway()
    Includes a test with a mockr mock of get_data_object.

--- tests/testthat/test-tables.R
+++ tests/testthat/test-tables.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("get_gene_symbols_for_pathway returns gene symbols", {
+  fake_get_data_object <- function (pathway_id, dataset_name) {
+    id <- c('123')
+    data_set <- c('universal_pathways')
+    key <- c('gs_cat', 'gs_subcat', 'gene_symbol', 'gene_symbol', 'human_ensembl_gene')
+    value <- c('C5', 'GO:BP', 'ROCK1', 'ROCK2', 'ENSG00000163347')
+    tibble::as_tibble(data.frame(id, data_set, key, value))
+  }
+  local({
+    mockr::local_mock(get_data_object = fake_get_data_object)
+    gene_symbols <- get_gene_symbols_for_pathway('123')
+    expect_equal(gene_symbols, c('ROCK1', "ROCK2"))
+  })
 })
